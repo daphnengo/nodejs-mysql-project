aea1e05333dfc579ea7de34c86420146524ad3d3
Daphne Ngo <dn.ngo29@gmail.com>
2024-07-20 02:45:39 -0400

    getPostEditProduct

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,3 +37,37 @@ exports.getManageProducts = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const productId = req.params.productId;
+
+  Product.findProductById(productId)
+    .then(([product]) => {
+      console.log(product);
+      res.render('admin/add-edit-product.html', {
+        pageTitle: 'Admin Edit Product',
+        path: '/admin/edit-product',
+        btnLabel: 'Update Product',
+        pathParam: 'edit-product',
+        product: product[0],
+        isEdit: true,
+      });
+    })
+    .catch(error => console.log(error));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const {
+    description,
+    imageUrl,
+    price,
+    title,
+    productId,
+  } = req.body;
+
+  const product = new Product(productId, title, imageUrl, price, description);
+
+  product.updateProduct()
+    .then(() => res.redirect('/admin/manage-products'))
+    .catch(error => console.log(error));
+};
